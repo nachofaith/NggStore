2eb429ce4190358a1a0b7428534a2bb490a3de4f
Nachin <ruben.godoy@ngg.cl>
2024-04-05 22:23:42 -0300

    Creando cards para productos

--- client/src/components/Ofertas.jsx
+++ client/src/components/Ofertas.jsx
@@ -1,8 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 function MostrarOfertas() {
   return (
     <>
-      <br />
-      <h1 className="text-center">Productos en oferta</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center">Productos en oferta</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title className="text-uppercase">
+                  Audifonos Astro A50
+                </Card.Title>
+                <Card.Text>$18.000</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </>
   );
 }
