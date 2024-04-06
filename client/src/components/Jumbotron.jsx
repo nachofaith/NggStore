327e316cb85a781c4f4147ebaa6c99dff19bd0cf
Nachin <ruben.godoy@ngg.cl>
2024-04-06 15:30:53 -0300

    Se deja rery footer y jumbotron, ademas de prod en oferta

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -2,14 +2,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import '/src/assets/custom.css';
-
-
+import "/src/assets/custom.css";
 
 function JumboTron() {
   return (
     <>
-     <Container className="border-bottom">
+      <Container className="border-bottom">
         <Row className="flex-lg-row-reverse align-items-center g-5 py-5">
           <Col sm={8} lg={6}>
             <img
@@ -39,47 +37,68 @@ function JumboTron() {
         </Row>
       </Container>
 
-      <Container>
+      <Container className="border-bottom">
         <Row>
+          <Col className="border-end">
+            <a id="img1" href="#" className="text-decoration-none">
+              <div className="h-100 p-5">
+                <h2 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-uppercase text-center">
+                  Mouse y Teclados Gamer
+                </h2>
+                <p className="lead text-center text-dark">
+                  Tenemos la mejor coleccion de Mouse y Teclados para llevar tu
+                  juego al siguiente nivel. en Ngg Store encontraras los mejores
+                  teclado mecanicos con las respuesta mas rapida como tambien
+                  los mejores Mouse con sus sensores y Dpi que te haran marcar
+                  la diferencia{" "}
+                </p>
 
-        <Col>
-        <a href="#" className="text-decoration-none">
+                <div className="overflow-hidden">
+                  <div className="container px-5">
+                    <img
+                      src="/public/img/slider1.png"
+                      className="img-fluid border rounded-3 mb-4"
+                      alt="Example image"
+                      width="700"
+                      height="500"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Col>
+          <Col>
+            <a id="img1" href="#" className="text-decoration-none">
+              <div className="h-100 p-5 ">
+                <h2 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-uppercase text-center">
+                  Mouse y Teclados Gamer
+                </h2>
+                <p className="lead text-center text-dark">
+                  Tenemos la mejor coleccion de Mouse y Teclados para llevar tu
+                  juego al siguiente nivel. en Ngg Store encontraras los mejores
+                  teclado mecanicos con las respuesta mas rapida como tambien
+                  los mejores Mouse con sus sensores y Dpi que te haran marcar
+                  la diferencia{" "}
+                </p>
 
-        <div className="h-100 p-5">
-          <h2 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-uppercase text-center">Mouse y Teclados Gamer
-          </h2>
-          <p className="lead text-center text-dark">Tenemos la mejor coleccion de Mouse y Teclados para llevar tu juego al siguiente nivel. en Ngg Store encontraras los mejores teclado mecanicos con las respuesta mas rapida como tambien los mejores Mouse con sus sensores y Dpi que te haran marcar la diferencia </p>
-  
-          <div className="overflow-hidden">
-            <div className="container px-5">
-              <img src="/public/img/slider1.png" className="img-fluid border rounded-3 mb-4" alt="Example image" width="700" height="500" loading="lazy" />
-            </div>
-          </div>
-        </div>
-       
-       </a>
-        
-        </Col>
-        <Col>
-        <a href="#" className="text-decoration-none">
-        <div className="h-100 p-5 ">
-          <h2 className="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-uppercase text-center">Mouse y Teclados Gamer
-          </h2>
-          <p className="lead text-center text-dark">Tenemos la mejor coleccion de Mouse y Teclados para llevar tu juego al siguiente nivel. en Ngg Store encontraras los mejores teclado mecanicos con las respuesta mas rapida como tambien los mejores Mouse con sus sensores y Dpi que te haran marcar la diferencia </p>
-  
-          <div className="overflow-hidden">
-            <div className="container px-5">
-              <img src="/public/img/slider1.png" className="img-fluid border rounded-3 mb-4" alt="Example image" width="700" height="500" loading="lazy" />
-            </div>
-          </div>
-        </div>
-      </a>
-
-        
-        </Col>
+                <div className="overflow-hidden">
+                  <div className="container px-5">
+                    <img
+                      src="/public/img/slider1.png"
+                      className="img-fluid border rounded-3 mb-4"
+                      alt="Example image"
+                      width="700"
+                      height="500"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Col>
         </Row>
       </Container>
-      
     </>
   );
 }
--- client/src/components/Ofertas.jsx
+++ client/src/components/Ofertas.jsx
@@ -3,13 +3,11 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Producto from "./Producto";
 import Badge from "react-bootstrap/Badge";
-import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 
 function MostrarOfertas() {
   return (
     <>
+    <br />
       <Container>
         <Row>
           <Col>
